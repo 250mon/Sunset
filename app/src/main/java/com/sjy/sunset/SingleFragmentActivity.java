00814ce62897f8aefcc9938f245d3329827f815b
250mon <lambki@naver.com>
2020-01-29 06:59:12 +0900

    complete

---
+++ app/src/main/java/com/sjy/sunset/SingleFragmentActivity.java
@@ -0,0 +1,26 @@
+package com.sjy.sunset;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+
+        }
+    }
+}
---
+++ app/src/main/java/com/sjy/sunset/SunsetActivity.java
@@ -0,0 +1,10 @@
+package com.sjy.sunset;
+
+import androidx.fragment.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
